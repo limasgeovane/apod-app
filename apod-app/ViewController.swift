8c29978fb79b5cde5bb6a82faeabecb13481180a
Geovane Lima dos Santos <limasgeovane@MacBook-Air-de-Lazaro.local>
2025-01-30 10:54:57 -0300

    First commit

---
+++ apod-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apod-app
+//
+//  Created by Geovane Lima dos Santos on 30/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ apod-appUITests/apod_appUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  apod_appUITestsLaunchTests.swift
+//  apod-appUITests
+//
+//  Created by Geovane Lima dos Santos on 30/01/25.
+//
+
+import XCTest
+
+final class apod_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
